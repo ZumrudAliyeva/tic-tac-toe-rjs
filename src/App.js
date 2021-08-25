bda5928934f9dac4d294faae5c2a52ba794b2fc4
ZumrudAliyeva <zumrudka@code.edu.az>
2021-08-25 12:12:50 +0400

    correction_history

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, {useState} from 'react'
 import Board from './components/Board';
+import History from './components/History';
 import { calculateWinner } from './helpers';
 import './styles/root.scss'
 
 function App() {
 
-  const [board, setBoard] = useState(Array(9).fill(null));
-  const [isXNext, setIsXNext] = useState(false);
+  // const [board, setBoard] = useState(Array(9).fill(null));
+  // const [isXNext, setIsXNext] = useState(false);
   const [history, setHistory] = useState([{board: Array(9).fill(null), isXNext: true}])
   const [currentMove, setCurrentMove] = useState(0);
   const current = history[currentMove]
@@ -18,7 +19,7 @@ const message = winner ? `Winner is ${winner}` :
 `Next player is ${current.isXNext ? 'x' : 'o'}`
 
   const handleSquareClick = (position)=>{
-  if(board[position] || winner){
+  if(current.board[position] || winner){
       return;
   }
   
@@ -37,11 +38,16 @@ setCurrentMove(prev => prev +1);
 
   }
 
+const moveTo = move => {
+  setCurrentMove(move);
+}
+
   return (
     <div className="app">
 <h1>TIC TAC TOE</h1>
 <h2>{message}</h2>
 <Board board={current.board} handleSquareClick={handleSquareClick} />
+<History history={history} moveTo={moveTo} currentMove={currentMove}/>
     </div>
   );
 }
